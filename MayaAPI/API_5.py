0afdf286ea0624d65b6077f207efd6920eba01e9
Yixiong Xu <328665042@qq.com>
2018-04-17 21:46:43 +0800

    Dirty Propagation, Push & Pull Mechanism, Lazy Evaluation, Updation Request

--- MayaAPI/API_5.py
+++ MayaAPI/API_5.py
@@ -1,6 +1,8 @@
 """
-Dirty Propagation
-Push & Pull Mechanism
-Lazy Evaluation
+Dirty Propagation :
+Push & Pull Mechanism: Sceen Mechanism. First from Input_Side to the Destination_Side, Push by the Dirty Propagation.
+                       Then, from the Destination_Side to Input_Side, Pull by the Updation Request
+Lazy Evaluation : maya does evaluation(compute()) action in Dependency node unless updating the Updation Request!
+Updation Request : ViewPort change, attribute Editor, Channel Box, cmds.getAttr()
 """
 # Clone from Home !

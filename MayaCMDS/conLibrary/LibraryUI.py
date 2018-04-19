c9aeafe816e4dcce5060ee748b69a7d23324efe8
Yixiong Xu <328665042@qq.com>
2018-04-19 11:32:32 +0800

    UI Library

--- MayaCMDS/conLibrary/LibraryUI.py
+++ MayaCMDS/conLibrary/LibraryUI.py
@@ -13,9 +13,40 @@ class ControllerLibraryUI(QtWidgets.QDialog):
         self.buildUI()
         self.populate()
 
-
     def buildUI(self):
-        pass
+
+        layout = QtWidgets.QVBoxLayout()
+
+        saveWidget = QtWidgets.QWidget()
+        saveLayout = QtWidgets.QHBoxLayout()
+        layout.addWidget(saveWidget)
+
+        self.saveNameField = QtWidgets.QLineEdit()
+        saveLayout.addWidget(self.saveNameFielde)
+
+        saveBtn = QtWidgets.QPushButton('Save')
+        saveLayout.addWidget(saveBtn)
+
+        self.listWidget = QtWidgets.QListWidget()
+        layout.addWidget(self.listWidget)
+
+        btnWidget = QtWidgets.QWidget()
+        btnLayout = QtWidgets.QHBoxLayout(btnWidget)
+        layout.addWidget(btnWidget)
+
+        importBtn = QtWidgets.QPushButton('import!')
+        btnLayout.addWidget(importBtn)
+
+        refreshBtn = QtWidgets.QPushButton('Refresh!')
+        btnLayout.addWidget(refreshBtn)
+
+        closeBtn = QtWidgets.QPushButton('Close')
+        btnLayout.addWidget(closeBtn)
+
+
+
+
+
 
     def populate(self):
         pass

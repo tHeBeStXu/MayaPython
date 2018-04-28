27160b441fba3b9f6e48e6a8d4e9f64f4415cda3
Yixiong Xu <328665042@qq.com>
2018-04-28 14:38:16 +0800

    Locator Node WIP(something is wrong)

--- MayaAPI/API_7.py
+++ MayaAPI/API_7.py
@@ -17,89 +17,63 @@ nodeName = 'LeftFoot'
 nodeID = openmaya.MTypeId(0x100fff)
 
 glRender = openmayarender.MHardwareRenderer.theRenderer()
-glFT = glRender.glFunctionTable()
-
-
-
+glFuncTable = glRender.glFunctionTable()
 
 class locatorNode(openmayampx.MPxLocatorNode):
+
+
     def __init__(self):
         super(locatorNode, self).__init__()
 
     def compute(self, plug, dataBlock):
         return openmaya.kUnknownParameter
 
-    def draw(self, view, dagPath, style, status):
+    def draw(self, view, path, style, status):
 
         view.beginGL()
         # Push the current state
-        glFT.glPushAttrib(openmayarender.MGL_CURRENT_BIT)
+        glFuncTable.glPushAttrib(openmayarender.MGL_CURRENT_BIT)
         # Enable Blend mode(to enable transparency)
-        glFT.glEnable(openmayarender.MGL_BLEND)
+        glFuncTable.glEnable(openmayarender.MGL_BLEND)
         # Defined Blend function
-        glFT.glBlendFunc(openmayarender.MGL_SRC_ALPHA, openmayarender.MGL_ONE_MINUS_SRC_ALPHA)
+        glFuncTable.glBlendFunc(openmayarender.MGL_SRC_ALPHA, openmayarender.MGL_ONE_MINUS_SRC_ALPHA)
 
         if status == view.kActive:
-            glFT.glColor4f(0.2, 0.5, 0.1, 0.3)
+            glFuncTable.glColor4f(0.2, 0.5, 0.1, 0.3)
         elif status == view.kLead:
-            glFT.glColor4f(0.5, 0.2, 0.1, 0.3)
+            glFuncTable.glColor4f(0.5, 0.2, 0.1, 0.3)
         elif status == view.kDormant:
-            glFT.glColor4f(0.1, 0.1, 0.1, 0.3)
+            glFuncTable.glColor4f(0.1, 0.1, 0.1, 0.3)
 
-        # Draw shape
-        glFT.glBegin(openmayarender.MGL_POLYGON)
-        glFT.glVertex3f()
+        # Draw Polygon shape
+        glFuncTable.glBegin(openmayarender.MGL_POLYGON)
+        glFuncTable.glVertex3f(-0.031, 0, -2.875)
+        glFuncTable.glVertex3f(-0.939, 0.1, -2.370)
+        glFuncTable.glVertex3f(-1.175, 0.2, -1.731)
+        glFuncTable.glVertex3f(-0.603, 0.3, 1.060)
+        glFuncTable.glVertex3f(0.473, 0.3, 1.026)
+        glFuncTable.glVertex3f(0.977, 0.2, -1.731)
+        glFuncTable.glVertex3f(0.809, 0.1, -2.337)
+        glFuncTable.glVertex3f(0.035, 0, -2.807)
 
+        view.drawText("Left Foot", openmaya.MPoint(0, 0, 0), view.kLeft)
 
-
-        glFT.glEnd()
+        glFuncTable.glEnd()
         # Disable blend shape
-        glFT.glDisable(openmayarender.MGL_BLEND)
+        glFuncTable.glDisable(openmayarender.MGL_BLEND)
         # Restore the state
-        glFT.glPopAttrib()
+        glFuncTable.glPopAttrib()
 
         view.endGL()
-
-
-
-
-
-
-
 
 
 # 1. Creator Function step
 def nodeCreator():
     return openmayampx.asMPxPtr(locatorNode())
 
-
 # 2. Initialize step
 def nodeInitializer():
-    # 1. creating a functionSet for numeric attributes
-    mFnAttr = openmaya.MFnNumericAttribute()
-
-    # 2. create the attributes
-    # 2.1 create the attributes, return MObject handle
-
-    wheelNode.inRadius = mFnAttr.create('radius', 'r', openmaya.MFnNumericData.kFloat, 0.0)
-    # 2.2 set the properties
-    # readable -> source , output
-    # writable -> destination, input
-    # storable -> store with file
-    # keyable -> can be key or not
-    mFnAttr.setReadable(1)
-    mFnAttr.setWritable(1)
-    mFnAttr.setStorable(1)
-    mFnAttr.setKeyable(1)
-
-    # 3. attach attributes to the node:
-    wheelNode.addAttribute(wheelNode.inRadius)
-    wheelNode.addAttribute(wheelNode.inTranslate)
-    wheelNode.addAttribute(wheelNode.outRotate)
-
-    # 4. design circuitry
-    wheelNode.attributeAffects(wheelNode.inRadius, wheelNode.outRotate)
-
+    pass
 
 # 3. initialize Plugin
 def initializePlugin(mObj):

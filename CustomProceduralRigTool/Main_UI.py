37c5f6d34b3af39b220a7adb3bb7b5e3974b4723
Yixiong Xu <328665042@qq.com>
2018-05-07 18:37:44 +0800

    Custom Procedural Rig Tool WIP

--- CustomProceduralRigTool/Main_UI.py
+++ CustomProceduralRigTool/Main_UI.py
@@ -97,6 +97,11 @@ class RiggingMainUI(QtWidgets.QWidget):
         layout.addWidget(addBtn, 0, 2)
 
         # Rig File Name
+        proNameLabel = QtWidgets.QLabel('Rig Name: ')
+        proNameLabel.setAlignment(QtCore.Qt.AlignCenter)
+        proNameLineEdit = QtWidgets.QLineEdit('Procedural Rig')
+        layout.addWidget(proNameLabel, 1, 0)
+        layout.addWidget(proNameLineEdit, 1, 1, 1, 2)
 
         # Scroll Widget
         scrollWidget = QtWidgets.QWidget()
@@ -114,14 +119,25 @@ class RiggingMainUI(QtWidgets.QWidget):
         layout.addWidget(saveBtn, 3, 0)
 
         # Import Button
-        importBtn = QtWidgets.QPushButton('Import')
+        importBtn = QtWidgets.QPushButton('Import Rig')
         importBtn.clicked.connect(self.importRig)
         layout.addWidget(importBtn, 3, 1)
 
         # Create Rig Button
-        createBtn = QtWidgets.QPushButton('Create')
+        createBtn = QtWidgets.QPushButton('Create Rig!')
         createBtn.clicked.connect(self.createRig)
         layout.addWidget(createBtn, 3, 2)
+
+        # Skin row
+        skinWidget = QtWidgets.QWidget()
+        skinLayout = QtWidgets.QHBoxLayout()
+        skinWidget.setLayout(skinLayout)
+        layout.addWidget(skinWidget, 4, 0, 1, 3)
+        skinExportBtn = QtWidgets.QPushButton('Export Weights')
+        skinImportBtn = QtWidgets.QPushButton('Import Weights')
+        skinLayout.addWidget(skinExportBtn)
+        skinLayout.addWidget(skinImportBtn)
+
 
     def createRig(self):
         self.saveRig()

72a94e00c3209f10be6b4df381bf1ece86a51eec
Yixiong Xu <328665042@qq.com>
2018-06-04 18:33:14 +0800

    Custom Exporter WIP UI model proc WIP

--- CustomExporter/UI.py
+++ CustomExporter/UI.py
@@ -1,9 +1,16 @@
+from functools import partial
+
 import maya.cmds as cmds
 
 
-class MainUI():
-    def __init__(self):
+from CustomExporter import base, fbxExport
 
+
+class MainUI(object):
+
+
+
+    def __init__(self):
         self.buildUI()
 
     def buildUI(self):
@@ -18,8 +25,8 @@ class MainUI():
         cmds.menuItem(parent=self.editMenu, label='Reset Settings')
 
         self.helpMenu = cmds.menu(label='Help')
-        cmds.menuItem(parent=self.helpMenu, label='Help on Animation Export')
-        cmds.menuItem(parent=self.helpMenu, label='Help on Export')
+        cmds.menuItem(parent=self.helpMenu, label='Help on Animation Export', command=self.animHelpWindow)
+        cmds.menuItem(parent=self.helpMenu, label='Help on Export', command=self.modelHelpWindow)
 
         # tab layout
         self.formLayout = cmds.formLayout()
@@ -115,15 +122,25 @@ class MainUI():
         cmds.formLayout(self.animationFormLayout, edit=1, attachControl=[(self.animExportSelectedAnimationBtn, 'left', 100, self.animExportNodesTextScrollList),
                                                                          (self.animAllAnimationForSelectedCharacterBtn, 'left', 100, self.animExportNodesTextScrollList),
                                                                          (self.animAllAnimationBtn, 'left', 100, self.animExportNodesTextScrollList)])
+        # Animation Popup Menu
+        self.animExportNodesPopupMenu = cmds.popupMenu(button=3, parent=self.animExportNodesTextScrollList)
+        self.animSelectNodeMenuItem = cmds.menuItem(label='Select', parent=self.animExportNodesPopupMenu)
+        self.animRenameNodeMenuItem = cmds.menuItem(label='Rename', parent=self.animExportNodesPopupMenu)
+        self.animDeleteNodeMenuItem = cmds.menuItem(label='Delete', parent=self.animExportNodesPopupMenu)
+
+
         # create model UI elements
         self.modelOriginTextScrollList = cmds.textScrollList(width=175, height=220, numberOfRows=18,
-                                                             allowMultiSelection=0, parent=self.modelFormLayout)
+                                                             allowMultiSelection=0, parent=self.modelFormLayout,
+                                                             sc=self.populateModelExportPanel)
         self.modelExportNodesTextScrollList = cmds.textScrollList(width=175, height=220, numberOfRows=18,
                                                                   allowMultiSelection=0, parent=self.modelFormLayout)
         self.modelGeomTextScrollList = cmds.textScrollList(width=175, height=220, numberOfRows=18, allowMultiSelection=1,
                                                            parent=self.modelFormLayout)
-        self.modelTagAsOriginBtn = cmds.button(width=175, height=50, label='Tag as Origin', parent=self.modelFormLayout)
-        self.modelNewExportNodeBtn = cmds.button(width=175, height=50, label='New Export Node', parent=self.modelFormLayout)
+        self.modelTagAsOriginBtn = cmds.button(width=175, height=50, label='Tag as Origin', parent=self.modelFormLayout,
+                                               command=self.modelTagAsOrigin)
+        self.modelNewExportNodeBtn = cmds.button(width=175, height=50, label='New Export Node', parent=self.modelFormLayout,
+                                                 command=self.modelCreateNewExportNode)
         self.modelAddRemoveMeshesBtn = cmds.button(width=175, height=50, label='Add / Remove Meshes', parent=self.modelFormLayout)
         self.modelExportCheckBoxGrp = cmds.checkBoxGrp(numberOfCheckBoxes=1, label='Export', columnWidth2=[85, 70], enable=0,
                                                        parent=self.modelFormLayout)
@@ -135,6 +152,7 @@ class MainUI():
                                                                              buttonLabel='Browse...', parent=self.modelFormLayout)
         self.modelExportMeshBtn = cmds.button(width=175, height=50, label='Export Selected Character', parent=self.modelFormLayout)
         self.modelExportAllMeshesBtn = cmds.button(width=175, height=50, label='Export All Characters', parent=self.modelFormLayout)
+
 
         # set up model form layout
         cmds.formLayout(self.modelFormLayout, edit=1, attachForm=[(self.modelOriginText, 'top', 5),
@@ -162,8 +180,100 @@ class MainUI():
         cmds.formLayout(self.modelFormLayout, edit=1, attachControl=[(self.modelExportFileNameTextFieldButtonGrp, 'top', 5, self.modelExportCheckBoxGrp),
                                                                      (self.modelExportCheckBoxGrp, 'left', 125, self.modelGeomTextScrollList)])
 
+        # Model Popup Menu
+        self.modelExportNodesPopupMenu = cmds.popupMenu(button=3, parent=self.modelExportNodesTextScrollList)
+        self.modelSelectMenuItem = cmds.menuItem(label='Select', parent=self.modelExportNodesPopupMenu)
+        self.modelRenameMenuItem = cmds.menuItem(label='Rename', parent=self.modelExportNodesPopupMenu)
+        self.modelDeleteMenuItem = cmds.menuItem(label='Delete', parent=self.modelExportNodesPopupMenu)
+
         # set up tab
         cmds.tabLayout(self.tabLayout, edit=1, tabLabel=((self.animationFrameLayout, 'Animation'),
                                                          (self.modelFrameLayout, 'Model')))
 
+        # Populate UI
+        self.populateModelRootJointsPanel()
+        self.populateAnimActorPanel()
+
+        # scriptJob to refresh it
+        cmds.scriptJob(parent=self.mainWindow, e=['PostSceneRead', "import CustomExporter\nCustomExporter.UI.MainUI().populateModelRootJointsPanel()"])
+        cmds.scriptJob(parent=self.mainWindow, e=['PostSceneRead', "import CustomExporter\nCustomExporter.UI.MainUI().populateAnimActorPanel()"])
+
         cmds.showWindow(self.mainWindow)
+
+
+    # help on animation
+    def animHelpWindow(self, *args):
+        if cmds.window('animHelpWindowUI', exists=1):
+            cmds.deleteUI('animHelpWindowUI')
+
+        self.animHelpWindowUI = cmds.window('animHelpWindowUI', s=1, width=500, height=500, menuBar=1, title='Help on Animation Export')
+        cmds.paneLayout(configuration='horizontal4')
+        cmds.scrollField(editable=0, wordWrap=1, text="Animation Export: \nAnimation export assumes single-level referencing with proper namesapce.\n\nActors: \nAll referenced characters with a origin joint tagged with the origin attribute will be listed in the Actor's field by their namespace. Please see the modeling help window for how to tage a character's origin with the origin attribute.\n\nExport Nodes:\nThe Export Nodes panel will fill in with export nodes connected to the origin of the selected actor from the Actor's field. Clicking on the New Export Node will create a new node. Each export node represents a seperate animation.\n\nExport:\nThe Export flag means the current export ndoe will be available for export. All nodes wihtout this checked will not be exported.\n\nMove to origin:\nNot yet supported\n\nSub Range:\nTurn this on to enable the sub-range option for the selected node. This will enable the Start Frame and End Frame fields where you can set the range for the specified animation. Otherwise, the animation will use the frame range of the file.\n\nExport File Name:\nClick on the Browse button to browse to where you want the file to go. The path will be project relative.\n\nExport Selected Animation:\nClick this button to export the animation selected in Export Nodes\n\nExport All Animations For Selected Character:\nClick this button to export all animations for the selected actor in the Actors filed. This flag will ignore what is selected in Export Nodes and export from all found nodes for the character\n\nExport All Animations:\nClick this button to export all animations for all characters. All selections will be ignored")
+        cmds.showWindow('animHelpWindowUI')
+
+    # help on model
+    def modelHelpWindow(self, *args):
+        if cmds.window('modelHelpWindowUI', exists=1):
+            cmds.deleteUI('modelHelpWindowUI')
+
+        self.modelHelpWindowUI = cmds.window('modelHelpWindowUI', s=1, width=500, height=500, menuBar=1, title='Help on Model Export')
+        cmds.paneLayout(configuration='horizontal4')
+        cmds.scrollField(editable=0, wordWrap=1, text="Model Export: \nModel exporter assumes one skeleton for export. Referencing for model export is not supported\n\nRoot Joints: \nPanel will list all the joints tagged with the \"origin\" attribute. If no joint is tagged with the attribute, it will list all joints in the scene and turn red. Select the root joint and click the Tag as Origin button.\n\nExport Nodes:\nThe Export Nodes panel will fill in with export nodes connected to the origin of the selected actor from the Actor's field. Clicking on the New Export Node will create a new node. Each export node represents a seperate character export (for example, seperate LOD's).\n\nMeshes:\nThe Meshes panel shows all the geometry associated with the selected export node. This can be used if you have mesh variations skinned to the same rig or LOD's.\n\nExport File Name:\nClick on the Browse button to browse to where you want the file to go. The path will be project relative.\n\nExport Selected Character:\nClick this button to export the character selected in Export Nodes\n\nExport All Characters:\nClick this button to export all character definitions for the skeleton. All selections will be ignored")
+        cmds.showWindow(self.modelHelpWindowUI)
+
+    # model ui proc
+    def populateModelRootJointsPanel(self):
+        cmds.textScrollList(self.modelOriginTextScrollList, edit=1, removeAll=1)
+
+        origin = base.returnOrigin('')
+
+        if origin != 'Error':
+            cmds.textScrollList(self.modelOriginTextScrollList, edit=1, ebg=0, append=origin)
+        else:
+            joints = cmds.ls(type='joint')
+            for curJoint in joints:
+                cmds.textScrollList(self.modelOriginTextScrollList, edit=1, bgc=[1, 0.1, 0.1], append=curJoint)
+
+    def modelTagAsOrigin(self, *args):
+        joints = cmds.textScrollList(self.modelOriginTextScrollList, q=1, selectItem=1)
+        base.tagForOrigin(joints[0])
+        self.populateModelRootJointsPanel()
+
+    def populateModelExportPanel(self, *args):
+        origin = cmds.textScrollList(self.modelOriginTextScrollList, q=1, selectItem=1)
+        cmds.textScrollList(self.modelExportNodesTextScrollList, edit=1, removeAll=1)
+
+        if origin:
+
+            exportNodes = fbxExport.returnFBXExportNodes(origin[0])
+
+            if exportNodes:
+                for cur in exportNodes:
+                    cmds.textScrollList(self.modelExportNodesTextScrollList, edit=1, append=cur)
+
+    def modelCreateNewExportNode(self, *args):
+        origin = cmds.textScrollList(self.modelOriginTextScrollList, q=1, selectItem=1)
+
+        if origin[0] != 'Error':
+            exportNode = fbxExport.createFBXExportNode(origin[0])
+
+            if exportNode:
+                fbxExport.connectFBXExportNodeToOrigin(exportNode, origin[0])
+                self.populateModelExportPanel()
+
+
+
+    # anim ui proc
+    def populateAnimActorPanel(self):
+        cmds.textScrollList(self.animActorsTextScrollList, edit=1, removeAll=1)
+
+        reference = cmds.file(query=1, reference=1)
+
+        for curRef in reference:
+            if not cmds.file(curRef, query=1, deferReference=1):
+                ns = cmds.file(curRef, query=1, namespace=1)
+                origin = base.returnOrigin(ns)
+
+                if origin != 'Error':
+                    cmds.textScrollList(self.animActorsTextScrollList, edit=1, append=ns)
+

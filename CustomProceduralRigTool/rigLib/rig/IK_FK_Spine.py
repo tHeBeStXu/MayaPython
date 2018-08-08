cc5a389a3a47cb962880ceefb5f5cf5c0b99f2bc
Yixiong Xu <328665042@qq.com>
2018-08-08 18:43:34 +0800

    IK_FK_Spine rig

--- CustomProceduralRigTool/rigLib/rig/IK_FK_Spine.py
+++ CustomProceduralRigTool/rigLib/rig/IK_FK_Spine.py
@@ -9,7 +9,7 @@ def build(spineJoints,
           prefix,
           rigScale,
           numFK_Jnt=3,
-          baseRig=None):
+      baseRig=None):
     """
     Build IK_FK_Spine rig.
     This IK_FK_Spine build module is used for spine joint chain which is placed in the 0 x-Axis position.

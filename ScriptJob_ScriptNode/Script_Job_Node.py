705f377540926f24d83e7afa09f88dab1788696b
Yixiong Xu <328665042@qq.com>
2018-08-15 17:59:58 +0800

    ScriptJob and ScriptNode combine

---
+++ ScriptJob_ScriptNode/Script_Job_Node.py
@@ -0,0 +1,20 @@
+import maya.cmds as cmds
+
+codeStr = '''
+def colorChange():
+
+    objAttr = 'pSphere1.Color'
+
+    if cmds.getAttr(objAttr) == 0:
+        cmds.polyColorPerVertex(r=0.0, g=1.0, b=0.0, a=1.0, cdo=1)
+    elif cmds.getAttr(objAttr) == 1:
+        cmds.polyColorPerVertex(r=0.0, g=0.0, b=1.0, a=1.0, cdo=1)
+    elif cmds.getAttr(objAttr) == 2:
+        cmds.polyColorPerVertex(r=1.0, g=0.0, b=0.0, a=1.0, cdo=1)
+
+cmds.scriptJob(attributeChange=['pSphere1.Color', colorChange])
+'''
+
+cmds.scriptNode(st=2, bs=codeStr.replace("'''", "''"), n='sphere_ColorChange', stp='python')
+
+

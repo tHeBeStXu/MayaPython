d0d2fe34589f52d0a0037318f15c1a796e3da390
Yixiong Xu <328665042@qq.com>
2018-08-16 18:31:51 +0800

    IK_FK Seamless Switch

--- ScriptJob_ScriptNode/IK_FK_Switch.py
+++ ScriptJob_ScriptNode/IK_FK_Switch.py
@@ -10,22 +10,31 @@ def IK_FK_Switch(prefix,
     codeStr = '''
 import maya.cmds as cmds
 
-def switch(switchCtrl=%s, pvCtrl=%s, ikCtrl=%s, skinJoints=%s, fkCtrlList=%s):
+def switch():
+    switchCtrl = '{0}'
+    pvCtrl = '{1}'
+    ikCtrl = '{2}'
+    skinJoints = {3}
+    fkCtrlList = {4}
+
     objAttr = switchCtrl + '.Mode'
     objTy = switchCtrl + '.ty'
     # FK 2 IK
     if cmds.getAttr(objAttr) == 0:
-        cmds.setAttr(pvCtrl + '.t', cmds.getAttr(skinJoints[1] + '.t'))
-        cmds.setAttr(ikCtrl + '.t', cmds.getAttr(skinJoints[-1] + '.t'))
-        cmds.setAttr(ikCtrl + '.r', cmds.getAttr(skinJoints[-1] + '.r'))
+        cmds.matchTransform(pvCtrl, skinJoints[1], pos=1, rot=0)
+        cmds.matchTransform(ikCtrl, skinJoints[-1], pos=1, rot=1)
         cmds.setAttr(objTy, 1)
-
     # IK 2 FK
     elif cmds.getAttr(objAttr) == 1:
         for i in xrange(len(skinJoints)):
-            cmds.setAttr(fkCtrlList[i] + '.r', skinJoints[i] + '.r')
+            cmds.matchTransform(fkCtrlList[i], skinJoints[i], pos=1, rot=1)
         cmds.setAttr(objTy, 0)
 
-cmds.scriptJob(attributeChange=[objAttr, switch])
-''' % (switchCtrl, pvCtrl, ikCtrl, skinJoints, fkCtrlList)
+cmds.scriptJob(attributeChange=['{5}' + '.Mode', switch])
+'''.format(switchCtrl,
+           pvCtrl,
+           ikCtrl,
+           skinJoints,
+           fkCtrlList,
+           switchCtrl)
     cmds.scriptNode(st=2, bs=codeStr.replace("'''", "''"), n=prefix + 'Arm_IKFK_SwitchMode', stp='python')

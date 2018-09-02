4e2577b4c49ff1febaa8bf0398f753e6aeff28ba
Yixiong Xu <328665042@qq.com>
2018-09-03 01:35:09 +0800

    update

--- CustomProceduralRigTool/rigLib/rig/__init__.py
+++ CustomProceduralRigTool/rigLib/rig/__init__.py
@@ -1,12 +1,12 @@
 import IK_HumanLeg
 import IK_FK_Spine
-import IK_FK_Arm
+import IK_FK_HumanArm
 import IK_AnimalLeg
 import Slaver_Joints
 import Blend_RollChain
 import IK_FK_Head_Neck
 import FK_Tail
-reload(IK_FK_Arm)
+reload(IK_FK_HumanArm)
 reload(IK_FK_Spine)
 reload(IK_AnimalLeg)
 reload(IK_FK_Head_Neck)

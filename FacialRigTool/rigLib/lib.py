0fafea7e60091b33d9b12453561bd6ca20418d9d
Yixiong Xu <328665042@qq.com>
2018-09-27 01:10:39 +0800

    update

---
+++ FacialRigTool/rigLib/lib.py
@@ -0,0 +1,13 @@
+import maya.cmds as cmds
+from ..base import control
+from ..base import module
+reload(control)
+reload(module)
+
+
+def curve2Joint(curve,
+                numJnt=1,
+                controlShape='circleX',
+                ):
+
+    pass

328c4fcc948089b5147367de4d785f5e119c11d4
Yixiong Xu <328665042@qq.com>
2018-10-22 19:14:57 +0800

    update

--- FacialRigTool/rig/singleLineRig.py
+++ FacialRigTool/rig/singleLineRig.py
@@ -14,7 +14,7 @@ def createRig(selectedLines,
               numJnt = 3,
               rigScale=1.0,
               addSliderCtrls=True,
-              ):
+              jointParent=''):
 
     cmds.select(cl=1)
 
@@ -24,12 +24,20 @@ def createRig(selectedLines,
 
     # create line
     targetLine = lib.createCurve(edgeList=selectedLines)
+    cmds.xform(targetLine, cp=1)
 
     # create joint along the curve
     jointList = lib.joint2Curve(prefix=prefix,
                                 partName=rigPartName,
                                 curve=targetLine,
                                 numJnt=numJnt)
+    # parent created joint to target joint
+    if jointParent:
+        for i in jointList:
+            cmds.select(cl=1)
+            cmds.parent(i, jointParent)
+
+    cmds.select(cl=1)
 
     # create controller for each joint
     jointCtrlList = []
@@ -37,15 +45,24 @@ def createRig(selectedLines,
     for i in jointList:
         jointCtrl = control.Control(prefix=i,
                                     rigPartName='',
-                                    scale=rigScale,
+                                    scale=rigScale * 0.2,
                                     translateTo=i,
                                     rotateTo=i,
                                     shape='circleY')
 
-        cmds.parent(i, jointCtrl.C)
+        cmds.pointConstraint(jointCtrl.C, i, mo=0)
+        cmds.orientConstraint(jointCtrl.C, i, mo=0)
 
         jointCtrlList.append(jointCtrl.C)
         jointCtrlGrpList.append(jointCtrl.Off)
+
+    # create module main control
+    mainCtrl = control.Control(prefix=prefix,
+                               rigPartName=rigPartName,
+                               scale=rigScale,
+                               translateTo=targetLine[0],
+                               rotateTo=jointList[0],
+                               shape='circleY')
 
     # connect the attribute
     for i in jointList:
@@ -53,23 +70,51 @@ def createRig(selectedLines,
                          i + '.' + prefix + rigPartName, f=1)
 
     # for each joint create the slide control
+    slideCtrlList = []
     slideCtrlGrpList = []
     if addSliderCtrls:
         for i in xrange(len(jointList)):
             sliderCtrl = control.Control(prefix=prefix,
                                          rigPartName=rigPartName + '_Jnt_' + str(i) + '_SLD',
-                                         scale=rigScale,
+                                         scale=rigScale * 0.5,
                                          shape='verticalSliderControl',
                                          lockChannels=['tx', 'ty', 'rx', 'ry', 'rz', 'sx', 'sy', 'sz', 'v'])
 
+            slideCtrlList.append(sliderCtrl.C)
             slideCtrlGrpList.append(sliderCtrl.Off)
 
-    # create a main Control for
-
+        # add main slider control
+        mainSliderCtrl = control.Control(prefix=prefix,
+                                         rigPartName=rigPartName + '_Jnt_Main_SLD',
+                                         scale=rigScale,
+                                         shape='verticalSliderControl',
+                                         lockChannels=['tx', 'ty', 'rx', 'ry', 'rz', 'sx', 'sy', 'sz', 'v'])
 
     # clean hierarchy
     cmds.parent(targetLine, rigModule.dontTouchGrp)
 
     for i in jointCtrlGrpList:
-        cmds.parent(i, rigModule.topGrp)
+        cmds.parent(i, mainCtrl.C)
+
+    cmds.parent(mainCtrl.Off, rigModule.topGrp)
+
+    if addSliderCtrls:
+        rigModuleSliderGrp = cmds.group(n=prefix + rigPartName + '_SLD_Grp', em=1)
+        for i in slideCtrlGrpList:
+            cmds.parent(i, rigModuleSliderGrp)
+
+        cmds.parent(mainSliderCtrl.Off, rigModuleSliderGrp)
+
+        cmds.parent(rigModuleSliderGrp, rigModule.topGrp)
+
+        # set default keyframe
+
+        cmds.setDrivenKeyframe(mainCtrl.C + '.translateZ', cd=mainSliderCtrl.C + '.translateZ')
+
+        for i in xrange(len(jointList)):
+            cmds.setDrivenKeyframe(jointCtrlList[i] + '.translateZ', cd=slideCtrlList[i] + '.translateZ')
+
+    cmds.select(cl=1)
+
+
 
--- FacialRigTool/rig/vertex2Rig.py
+++ FacialRigTool/rig/vertex2Rig.py
@@ -11,6 +11,7 @@ def createRig(vertexList,
               prefix='L_',
               rigPartName='',
               rigScale=1.0,
+              addSliderCtrls=True,
               jointParent=''):
 
     cmds.select(cl=1)
@@ -25,29 +26,72 @@ def createRig(vertexList,
                                   rigPartName=rigPartName,
                                   addSlaveAttr=True)
 
-    if not jointParent:
-        jointCtrlList = []
-        jointCtrlGrpList = []
-        # create controls
-        for i in xrange(len(jointList)):
-            jointCtrl = control.Control(prefix=jointList[i],
-                                        rigPartName='',
-                                        scale=rigScale,
-                                        translateTo=jointList[i],
-                                        rotateTo=jointList[i],
-                                        shape='circleY')
+    # parent created joint to target joint
+    if jointParent:
+        for i in jointList:
+            cmds.select(cl=1)
+            cmds.parent(i, jointParent)
+    cmds.select(cl=1)
 
-            cmds.parent(jointList[i], jointCtrl.C)
+    # add control for each joint
+    jointCtrlList = []
+    jointCtrlGrpList = []
+    # create controls
+    for i in xrange(len(jointList)):
+        jointCtrl = control.Control(prefix=jointList[i],
+                                    rigPartName='',
+                                    scale=rigScale * 0.2,
+                                    translateTo=jointList[i],
+                                    rotateTo=jointList[i],
+                                    shape='circleY')
 
-            jointCtrlList.append(jointCtrl.C)
-            jointCtrlGrpList.append(jointCtrl.Off)
+        cmds.pointConstraint(jointCtrl.C, jointList[i], mo=0)
+        cmds.orientConstraint(jointCtrl.C, jointList[i], mo=0)
+
+        jointCtrlList.append(jointCtrl.C)
+        jointCtrlGrpList.append(jointCtrl.Off)
 
     # connect the attribute
     for i in jointList:
         cmds.connectAttr(rigModule.topGrp + '.' + prefix + rigPartName + '_Jnt',
                          i + '.' + prefix + rigPartName, f=1)
 
+    # slider controls
+    slideCtrlList = []
+    slideCtrlGrpList = []
+
+    if addSliderCtrls:
+        for i in xrange(len(jointList)):
+            slideCtrl = control.Control(prefix=prefix,
+                                        rigPartName=rigPartName + '_Jnt_' + str(i) + '_SLD',
+                                        scale=rigScale * 0.5,
+                                        shape='planeSliderControl',
+                                        lockChannels=['ty', 'rx', 'ry', 'rz', 'sx', 'sy', 'sz', 'v'])
+
+            slideCtrlList.append(slideCtrl.C)
+            slideCtrlGrpList.append(slideCtrl.Off)
+
+        # add main slider control
+            mainSlideCtrl = control.Control(prefix=prefix,
+                                            rigPartName=rigPartName + '_Jnt_Main_SLD',
+                                            scale=rigScale,
+                                            shape='planeSliderControl',
+                                            lockChannels=['ty', 'rx', 'ry', 'rz', 'sx', 'sy', 'sz', 'v'])
+
     # cleanHierarchy
     for i in jointCtrlGrpList:
         cmds.parent(i, rigModule.topGrp)
+
+    cmds.parent(mainSlideCtrl.Off, rigModule.topGrp)
+
+    if addSliderCtrls:
+        rigModuleSliderGrp = cmds.group(n=prefix + rigPartName + '_SLD_Grp', em=1)
+        for i in slideCtrlGrpList:
+            cmds.parent(i, rigModuleSliderGrp)
+
+        cmds.parent(mainSlideCtrl.Off, rigModuleSliderGrp)
+        cmds.parent(rigModuleSliderGrp, rigModule.topGrp)
+
+        # set default keyframe
+        cmds.setDrivenKeyframe()
 

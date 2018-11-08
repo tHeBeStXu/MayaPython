34d782145bdab3ea048f792b5104929f5e8c8bbf
Yixiong Xu <328665042@qq.com>
2018-11-08 19:45:32 +0800

    lib WIP

--- nCloth2JointRigTool/rigLib/lib.py
+++ nCloth2JointRigTool/rigLib/lib.py
@@ -2,6 +2,24 @@ import maya.cmds as cmds
 from ..utils import name
 
 
+def createJoint(name):
+    cmds.select(cl=1)
+
+    joint = cmds.joint(n=name + '_Jnt_#')
+    cmds.select(cl=1)
+
+    if not cmds.attributeQuery('slaveJoint', node=joint, exists=1):
+        cmds.addAttr(joint, ln='slaveJoint', at='message')
+
+    if not cmds.attributeQuery('targetEmitter', node=joint, exists=1):
+        cmds.addAttr(joint, ln='targetEmitter', at='message')
+
+    if not cmds.attributeQuery('targetVertex', node=joint, exists=1):
+        cmds.addAttr(joint, ln='targetVertex', dt='string')
+
+    return {'joint': joint}
+
+
 def createEmitter(vertex,
                   meshName):
     """
@@ -12,10 +30,13 @@ def createEmitter(vertex,
 
     cmds.select(cl=1)
 
-    # create emitter on selected vertex
-
     cmds.select(vertex)
-    emitterList = cmds.emitter(n=vertex+'EM')
+    # create emitter on selected vertex
+    emitterList = cmds.emitter(n=vertex + '_EM', type='omni', r=100, sro=0, nuv=0, cye='none', cyi=1, spd=1, srn=0, nsp=1,
+                               tsp=0, mxd=0, mnd=0, dx=1, dy=0, dz=0, sp=0)
+    particle = cmds.particle(n=vertex + '_PTC')
+    cmds.connectDynamic(particle[0], em=emitterList[-1])
+    cmds.select(cl=1)
 
     emitter = emitterList[-1]
 
@@ -27,9 +48,14 @@ def createEmitter(vertex,
     if not cmds.attributeQuery('bindEmitter', node=meshName, exists=1):
         cmds.addAttr(meshName, ln='bindEmitter', at='message')
 
+    if not cmds.attributeQuery('targetJoint', node=emitter, exists=1):
+        cmds.addAttr(emitter, ln='targetJoint', exists=1)
+
     cmds.connectAttr(meshName + '.bindEmitter', emitter + '.bindMesh', f=1)
 
-    return {'emitter': emitter}
+    return {'emitter': emitter,
+            'particle': particle[0],
+            'particleShape': particle[-1]}
 
 
 def createNucleus(name):
@@ -41,14 +67,14 @@ def createNucleus(name):
     nucleus = cmds.createNode('nucleus', n=name + '_Nucleus')
 
     # connect attr
-    cmds.connectAttr('time.outTime', nucleus + '.currentTime')
+    cmds.connectAttr('time1.outTime', nucleus + '.currentTime')
 
     # add attr
     if not cmds.attributeQuery('nucleus', node=nucleus, exists=1):
         cmds.addAttr(nucleus, longName='nucleus', at='message', multi=1)
     cmds.select(cl=1)
 
-    return {'nucleus': nucleus}
+    return nucleus
 
 
 def createNCloth(name, nucleus=None):
@@ -97,7 +123,6 @@ def createNCloth(name, nucleus=None):
     return {'nucleus': nucleus,
             'nClothShape': nClothShape,
             'nClothTrans': nClothTrans}
-
 
 ########################################################################################################################
 # find same multi attr index method

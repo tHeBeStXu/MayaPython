399303ef6a520b5c8fa2a7ab7fd3fe6d8c715f40
Yixiong Xu <328665042@qq.com>
2018-11-09 19:58:00 +0800

    lib

--- nCloth2JointRigTool/rigLib/lib.py
+++ nCloth2JointRigTool/rigLib/lib.py
@@ -1,5 +1,62 @@
 import maya.cmds as cmds
-from ..utils import name
+import maya.mel as mel
+
+
+def createBlendSystem(simJointList, ctrlJointList, skinJointList, settingGrp):
+
+    for i in xrange(len(skinJointList)):
+
+        # translation
+        transBlendColor = cmds.createNode('blendColors', n='TransBlend_%s' % str(i))
+        # ctrlJoint
+        cmds.connectAttr(ctrlJointList[i] + '.t', transBlendColor + '.color1', f=1)
+        # simJoint
+        cmds.connectAttr(simJointList[i] + '.t', transBlendColor + '.color2', f=1)
+        # setting group
+        cmds.connectAttr(settingGrp + '.Sim2Bake', transBlendColor + '.blender', f=1)
+        # skinJoint
+        cmds.connectAttr(transBlendColor + '.output', skinJointList[i] + '.t', f=1)
+        cmds.select(cl=1)
+
+        # rotation
+        rotBlendColor = cmds.createNode('blendColors', n='RotBlend_%s' % str(i))
+        # ctrlJoint
+        cmds.connectAttr(ctrlJointList[i] + '.r', rotBlendColor + '.color1', f=1)
+        # simJoint
+        cmds.connectAttr(simJointList[i] + '.r', rotBlendColor + '.color2', f=1)
+        # setting group
+        cmds.connectAttr(settingGrp + '.Sim2Bake', rotBlendColor + '.blender', f=1)
+        # skinJoint
+        cmds.connectAttr(rotBlendColor + '.output', skinJointList[i] + '.r', f=1)
+        cmds.select(cl=1)
+
+
+def createSettingGrp(name):
+    settingGrp = cmds.group(n=name + '_SettingGrp', em=1)
+
+    # set visibility to false
+    cmds.setAttr(settingGrp + '.v', 0)
+
+    for attr in ['tx', 'ty', 'tz', 'rx', 'ry', 'rz', 'sx', 'sy', 'sz', 'v']:
+        cmds.setAttr(settingGrp + '.' + attr, keyable=0, lock=1, channelBox=0)
+
+    if not cmds.attributeQuery('Sim2Bake', node=settingGrp, exists=1):
+        cmds.addAttr(settingGrp, longName='Sim2Bake', at="float", min=0, max=1, keyable=1)
+
+    for attr in ['simJoint', 'skinJoint', 'ctrlJoint', 'bakeCtrl', 'bakeCtrlGrp', 'nucleus', 'nCloth']:
+        if not cmds.attributeQuery(attr, node=settingGrp, exists=1):
+            cmds.addAttr(settingGrp, ln=attr, at='message')
+
+    cmds.select(cl=1)
+    return settingGrp
+
+
+def placeJnt2Vert(vertex, joint):
+
+    vertexPos = cmds.xform(vertex, ws=1, q=1, t=1)
+    cmds.xform(joint, t=vertexPos, ws=1)
+
+    return vertexPos
 
 
 def createJoint(name):
@@ -20,8 +77,7 @@ def createJoint(name):
     return {'joint': joint}
 
 
-def createEmitter(vertex,
-                  meshName):
+def createEmitter(vertex, meshName):
     """
     create a point emitter
     :param vertex: str, vertex name
@@ -64,7 +120,7 @@ def createNucleus(name):
     :param name: str, name of the nucleus
     :return: nucleus
     """
-    nucleus = cmds.createNode('nucleus', n=name + '_Nucleus')
+    nucleus = cmds.createNode('nucleus', n=name + '_Nucleus_#')
 
     # connect attr
     cmds.connectAttr('time1.outTime', nucleus + '.currentTime')
@@ -77,39 +133,55 @@ def createNucleus(name):
     return nucleus
 
 
-def createNCloth(name, nucleus=None):
+def createNCloth(nClothMesh, nucleus=None):
     """
-    connect nucleus to nCloth
-    :param nucleus: str, nucleus
-    :param nCloth: str, nCloth shape
-    :return:
+    create nCloth on target mesh
+    :param nClothMesh: str, the nMesh you want to create nCloth on
+    :param nucleus: str, specified nucleus
+    :return: dict, {'nClothShape', 'nClothTrans', 'nucleus'}
     """
 
-    nucleus = nucleus or createNucleus(name=name)
-
-    nClothShape = cmds.createNode('nCloth')
     cmds.select(cl=1)
-    nClothTrans = cmds.listRelatives(nClothShape, c=0, p=1, s=0, type='transform')[0]
+    # create nCloth on selected mesh
+    cmds.select(nClothMesh)
+    nClothShape = mel.eval('createNCloth 0;')
 
-    nClothTrans = cmds.rename(nClothTrans, name + '_nCloth')
-    nClothShape = cmds.listRelatives(nClothTrans, c=1, p=0, s=1, type='nCloth')[0]
+    nClothTrans = cmds.listRelatives(nClothShape, p=1, s=0, c=0)[0]
+    nClothTrans = cmds.rename(nClothTrans, nClothMesh + '_nCloth')
+    nClothShape = cmds.listRelatives(nClothTrans, c=1, s=1, p=0)[0]
 
-    nucleusInputActiveIndex = findSingleAvailableIndex(nucleus + '.inputActive')
-    nucleusInputActiveAttr = nucleus + '.inputActive' + '[%s]' % nucleusInputActiveIndex
+    connectedNucleus = cmds.listConnections(nClothShape + '.startFrame', source=1, destination=0)
+    if connectedNucleus:
+        currentNucleus = cmds.rename(connectedNucleus[0], nClothMesh + '_Nucleus')
 
-    nucleusInputActiveStartIndex = findSingleAvailableIndex(nucleus + '.inputActiveStart')
-    nucleusInputActiveStartAttr = nucleus + '.inputActiveStart' + '[%s]' % nucleusInputActiveStartIndex
+    # not the taret nucleus
+    if nucleus:
+        if nucleus != currentNucleus:
+            # disconnect nCloth from current Nucleus
+            for attr in ['currentState', 'startState', 'nextState', 'startFrame']:
+                connectedAttr = cmds.listConnections(nClothShape + '.' + attr, connections=1, plugs=1)
+                try:
+                    cmds.disconnectAttr(connectedAttr[0], connectedAttr[-1])
+                except:
+                    cmds.disconnectAttr(connectedAttr[-1], connectedAttr[0])
 
-    nucleusOutputObjectsIndex = findSingleAvailableIndex(nucleus + '.outputObjects')
-    nucleusOutputObjectsAttr = nucleus + '.outputObjects' + '[%s]' % nucleusOutputObjectsIndex
+            # connect nCloth to target nucleus
+            for sourceAttr, destAttr in zip(['currentState', 'startState'], ['inputActive', 'inputActiveStart']):
+                index = findSingleAvailableIndex(nucleus + '.' + destAttr)
+                cmds.connectAttr(nClothShape + '.' + sourceAttr, nucleus + '.' + destAttr + '[%s]' % str(index), f=1)
 
-    # connect attr
-    cmds.connectAttr(nClothShape + '.currentState', nucleusInputActiveAttr, f=1)
-    cmds.connectAttr(nClothShape + '.startState', nucleusInputActiveStartAttr, f=1)
-    cmds.connectAttr(nucleus + '.startFrame', nClothShape + '.startFrame', f=1)
-    cmds.connectAttr(nucleusOutputObjectsAttr, nClothShape + '.nextState', f=1)
+            # connect nucleus to ncloth
+            for sourceAttr, destAttr in zip(['outputObjects', 'startFrame'], ['nextState', 'startFrame']):
+                if cmds.attributeQuery(sourceAttr, node=nucleus, multi=1):
+                    index = findSingleAvailableIndex(nucleus + '.' + sourceAttr)
+                    cmds.connectAttr(nucleus + '.' + sourceAttr + '[%s]' % str(index), nClothShape + '.' + destAttr, f=1)
+                else:
+                    cmds.connectAttr(nucleus + '.' + sourceAttr, nClothShape + '.' + destAttr, f=1)
 
-    cmds.connectAttr('time1.outTime', nClothShape + '.currentTime', f=1)
+            cmds.select(cl=1)
+
+    else:
+        nucleus = currentNucleus
 
     # add attr
     if not cmds.attributeQuery('nCloth', node=nClothShape, exists=1):
@@ -124,8 +196,9 @@ def createNCloth(name, nucleus=None):
             'nClothShape': nClothShape,
             'nClothTrans': nClothTrans}
 
-########################################################################################################################
-# find same multi attr index method
+#####################################
+# find same multi attr index method #
+#####################################
 
 
 def findSingleAvailableIndex(attr):

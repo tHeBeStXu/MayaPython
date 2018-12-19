b32add0f2ee1141ffb3b8f51ba9b1361b30f3fd0
Yixiong Xu <328665042@qq.com>
2018-12-19 22:15:44 +0800

    Transfer lattice deformer weights to skinCluster weights

---
+++ MayaAPI_2/Lattice2Skin.py
@@ -0,0 +1,76 @@
+import maya.api.OpenMaya as om
+import maya.api.OpenMayaAnim as oma
+import maya.cmds as cmds
+
+selectedLattice = 'ffd1Lattice'
+
+latticeShape = cmds.listRelatives(selectedLattice, type='lattice')[0]
+
+ffd = cmds.listConnections(latticeShape, type='ffd')[0]
+
+skinCluster = cmds.listConnections(latticeShape, type='skinCluster')[0]
+
+geometry = cmds.lattice(latticeShape, q=1, g=1)
+
+jointList = cmds.skinCluster(skinCluster, q=1, inf=1)
+
+
+meshMselection = om.MSelectionList()
+meshMselection.add(geometry)
+meshMDagPath = meshMselection.getDagPath(0)
+
+meshMFn = om.MFnMesh(meshMDagPath)
+
+geoPosition = meshMFn.getPoints(om.MSpace.kObject)
+
+weightList = []
+for index in range(len(jointList)):
+    jointMSelection = om.MSelectionList()
+    jointMSelection.add(jointList[index])
+
+    jointDagPath = jointMSelection.getDagPath(0)
+
+    mFnTransform = om.MFnTransform(jointDagPath)
+    world = mFnTransform.translation(om.MSpace.kWorld)
+    moveWorld = om.MVector(world.x + 1, world.y, world.z)
+
+    mFnTransform.setTranslation(moveWorld, om.MSpace.kWorld)
+
+    movePosition = meshMFn.getPoints(om.MSpace.kObject)
+    jointWeights = []
+    for vertexIndex in range(len(movePosition)):
+        length = movePosition[vertexIndex] - geoPosition[vertexIndex]
+        weight = length.length()
+        jointWeights.append(weight)
+
+    weightList.append(jointWeights)
+
+    mFnTransform.setTranslation(world, om.MSpace.kWorld)
+
+geoSkinCluster = cmds.skinCluster(jointList, geometry)[0]
+
+geoSkinMSelectionList = om.MSelectionList()
+geoSkinMSelectionList.add(geoSkinCluster)
+geoSkinMObject = geoSkinMSelectionList.getDependNode(0)
+
+mFnGeoSkinCluster = oma.MFnSkinCluster(geoSkinMObject)
+
+vertexIndexList = range(len(geoPosition))
+mFnIndexComponent = om.MFnSingleIndexedComponent()
+vertexComponent = mFnIndexComponent.create(om.MFn.kMeshVertComponent)
+mFnIndexComponent.addElements(vertexIndexList)
+
+influenceObjects = mFnGeoSkinCluster.influenceObjects()
+influenceIntList = om.MIntArray()
+for each in influenceObjects:
+    currentIndex = mFnGeoSkinCluster.indexForInfluenceObject(each)
+    influenceIntList.append(currentIndex)
+
+
+mWeightList = om.MDoubleArray()
+for wIndex in range(len(weightList[0])):
+    for jntIndex in range(len(weightList)):
+        mWeightList.append(weightList[jntIndex][wIndex])
+
+
+mFnGeoSkinCluster.setWeights(meshMDagPath, vertexComponent, influenceIntList, mWeightList)

760a98f77090449c7d8f9595e88fe46d4a7adfcb
Yixiong Xu <328665042@qq.com>
2019-01-03 19:26:17 +0800

    Plugin: All Intersection Point WIP

---
+++ MayaAPI/allIntersection.py
@@ -0,0 +1,111 @@
+import maya.api.OpenMaya as om2
+import maya.OpenMaya as om
+import maya.OpenMayaMPx as ompx
+
+nodeName = 'RayTracer'
+nodeID = om.MTypeId(0X100fff)
+
+
+class MeshIntersectionNode(ompx.MPxNode):
+    # Class Attrs
+    # INPUT
+    Loc_1_Pos = om.MObject()
+    Loc_2_Pos = om.MObject()
+
+    inMesh = om.MObject()
+
+    # OUTPUT
+    Loc_3_Pos = om.MObject()
+
+    def __init__(self):
+        ompx.MPxNode.__init__(self)
+
+    def compute(self, plug, dataBlock):
+        if plug != MeshIntersectionNode.Loc_3_Pos:
+            return om.kUnknownParameter
+
+        locator1Vector = om.MVector(dataBlock.inputValue(MeshIntersectionNode.Loc_1_Pos).asFloatVector())
+        locator2Vector = om.MVector(dataBlock.inputValue(MeshIntersectionNode.Loc_2_Pos).asFloatVector())
+        inputMeshObj = dataBlock.inputValue(MeshIntersectionNode.inMesh).asMesh()
+
+        direction = om.MFloatVector(locator2Vector - locator1Vector)
+
+        sourcePoint = om.MFloatPoint(dataBlock.inputValue(MeshIntersectionNode.Loc_1_Pos).asFloatVector())
+
+        inputMeshMFn = om.MFnMesh(inputMeshObj)
+
+        hitPoints = om.MFloatPointArray()
+
+        tolerance = om.MFloatArray()
+        tolerance.append(0.0)
+
+        inputMeshMFn.allIntersections(sourcePoint, direction, None, None, False, om.MSpace.kWorld,
+                                            10000.0, False, None, True, hitPoints, None, None, None, None, tolerance)
+
+        outputPoint = dataBlock.outputValue(MeshIntersectionNode.Loc_3_Pos)
+
+        if hitPoints[0]:
+            outputVector = om.MFloatVector(hitPoints[0].x, hitPoints[0].y, hitPoints[0].z)
+        else:
+            outputVector = om.MFloatVector(0.0, 0.0, 0.0)
+
+        outputPoint.setMFloatVector(outputVector)
+
+        dataBlock.setClean(plug)
+
+
+def nodeCreator():
+    return ompx.asMPxPtr(MeshIntersectionNode())
+
+
+def nodeInitializer():
+    MFnNumericAttr = om.MFnNumericAttribute()
+    MFnMeshAttribute = om.MFnTypedAttribute()
+
+    # create attribute
+    # input
+    MeshIntersectionNode.Loc_1_Pos = MFnNumericAttr.createPoint('Locator1Position', 'Loc1Pos')
+    MFnNumericAttr.setWritable(1)
+    MFnNumericAttr.setKeyable(1)
+    MeshIntersectionNode.addAttribute(MeshIntersectionNode.Loc_1_Pos)
+
+    MeshIntersectionNode.Loc_2_Pos = MFnNumericAttr.createPoint('Locator2Position', 'Loc2Pos')
+    MFnNumericAttr.setWritable(1)
+    MFnNumericAttr.setKeyable(1)
+    MeshIntersectionNode.addAttribute(MeshIntersectionNode.Loc_2_Pos)
+
+    MeshIntersectionNode.inMesh = MFnMeshAttribute.create('inMesh', 'im', om.MFnData.kMesh)
+    MFnMeshAttribute.setWritable(1)
+    MFnMeshAttribute.setKeyable(1)
+    MeshIntersectionNode.addAttribute(MeshIntersectionNode.inMesh)
+
+    # output
+    MeshIntersectionNode.Loc_3_Pos = MFnNumericAttr.createPoint('Locator3Position', 'Loc3Pos')
+    MFnNumericAttr.setWritable(0)
+    MFnNumericAttr.setReadable(1)
+    MeshIntersectionNode.addAttribute(MeshIntersectionNode.Loc_3_Pos)
+
+    # make influence
+    MeshIntersectionNode.attributeAffects(MeshIntersectionNode.Loc_1_Pos, MeshIntersectionNode.Loc_3_Pos)
+    MeshIntersectionNode.attributeAffects(MeshIntersectionNode.Loc_2_Pos, MeshIntersectionNode.Loc_3_Pos)
+    MeshIntersectionNode.attributeAffects(MeshIntersectionNode.inMesh, MeshIntersectionNode.Loc_3_Pos)
+
+
+def initializePlugin(mObj):
+    mPlugin = ompx.MFnPlugin(mObj, 'Yixiong Xu', '1.0')
+
+    try:
+        mPlugin.registerNode(nodeName, nodeID, nodeCreator, nodeInitializer)
+    except:
+        raise RuntimeError
+        print 'Failed to register node.'
+
+
+def uninitializePlugin(mObj):
+    mPlugin = ompx.MFnPlugin(mObj)
+
+    try:
+        mPlugin.deregisterNode(nodeID)
+    except:
+        raise RuntimeError
+        print 'Failed to deregister node.'

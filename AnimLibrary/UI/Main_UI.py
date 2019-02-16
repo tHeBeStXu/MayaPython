b4e591d38640f19d17f1569ad01e47ce43bb101c
Yixiong Xu <328665042@qq.com>
2019-02-17 00:46:13 +0800

    Main UI WIP

--- AnimLibrary/UI/Main_UI.py
+++ AnimLibrary/UI/Main_UI.py
@@ -6,6 +6,9 @@ import logging
 import os
 from functools import partial
 import shutil
+import imageio
+
+from ..Lib import exportAnimCurve
 
 logging.basicConfig()
 logger = logging.getLogger('AnimLibrary')
@@ -48,13 +51,16 @@ class MainUI(QtWidgets.QDialog):
         self.dataDir = '%s/Data' % self.rootDir
         self.tempDir = os.path.abspath(os.getenv('TEMP'))
         self.tempDir = self.tempDir.replace('\\', '/')
-        self.imagesDir = '%s/tempImages' % self.tempDir
+        self.tempGIFDir = '%s/tempGIF.gif' % self.tempDir
 
         # Current Mode
         self.currentMode = 'export'
 
-        # Current Pose Name
-        self.currentPoseData = ''
+        # Current anim Name
+        self.currentanimData = ''
+
+        # ButtonImageSize
+        self.size = [150, 150]
 
         # Build UI
         self.buildUI()
@@ -145,7 +151,7 @@ class MainUI(QtWidgets.QDialog):
         self.historyGroup.setLayout(historyLayout)
 
         self.historyEditLine = QtWidgets.QTextEdit()
-        self.historyEditLine.setFixedSize(160, 100)
+        self.historyEditLine.setFixedSize(160, 80)
         self.historyEditLine.setFocusPolicy(QtCore.Qt.NoFocus)
         self.historyEditLine.setReadOnly(True)
 
@@ -220,10 +226,10 @@ class MainUI(QtWidgets.QDialog):
         self.fileDir.itemClicked.connect(self.loadCurrentFolder)
 
         # Rename Anim
-        self.nameEditLine.returnPressed.connect(self.renamePose)
+        self.nameEditLine.returnPressed.connect(self.renameAnim)
 
         # Export Import Switch
-        self.poseExportImportSwitch(mode='export')
+        self.animExportImportSwitch(mode='export')
 
     def iconConfigure(self):
         """
@@ -244,7 +250,64 @@ class MainUI(QtWidgets.QDialog):
                 menuList[index].setIcon(icon)
 
     def loadFolderStructure(self, path):
-        pass
+        """
+        load folder structure of given path
+        :param path: str, self.libraryDirectory
+        :return: None
+        """
+        # first clear the existing file structure
+        self.fileDir.clear()
+
+        directoryList = self.getFolderStructure(path)
+
+        self.folderPath = path
+
+        self.loadFolder2TreeWidget(directoryList[path], self.fileDir, self.folderPath)
+
+    def getFolderStructure(self, path):
+        """
+        get folder structure with specified path
+        :param path: rootPath
+        :return: dict, directory list
+        """
+        directoryList = {}
+        for root, dirs, files in os.walk(path):
+            folderList = root.split(os.sep)
+
+            folders = directoryList
+
+            for eachFolder in folderList:
+                folders = folders.setdefault(eachFolder, {})
+        return directoryList
+
+    def loadFolder2TreeWidget(self, directoryList, parent, path):
+        """
+        load folder structure to treeWidget
+        :param directoryList: dict, directory list
+        :param parent: QtreeWidget, self.fileDir
+        :param path: file path
+        :return: None
+        """
+        for eachDirectory in directoryList:
+
+            self.folderPath = '%s/%s' % (path, eachDirectory)
+
+            item = QtWidgets.QTreeWidgetItem(parent)
+
+            item.setText(0, eachDirectory)
+
+            item.setToolTip(0,  self.folderPath)
+
+            item.toolTip(QtCore.Qt.WA_AlwaysShowToolTips)
+
+            # connect icon
+            icon = QtGui.QIcon()
+            icon.addPixmap(QtGui.QPixmap('%s/folder.png' % (self.iconsDir)), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            item.setIcon(0, icon)
+
+            self.loadFolder2TreeWidget(directoryList[eachDirectory], item, self.folderPath)
+
+        self.folderPath = path
 
     def createFolder(self):
         """
@@ -283,16 +346,122 @@ class MainUI(QtWidgets.QDialog):
                 os.makedirs('%s/%s' % (currentPath, str(folderName)))
 
     def expandFolder(self):
-        pass
+        """
+        Expand folder structure
+        :return: None
+        """
+        if self.fileDir.selectedItems():
+            currentItem = self.fileDir.selectedItems()[-1]
+            self.dependentList = [currentItem]
+            self.collectChildItems(currentItem)
+
+            for eachDepend in self.dependentList:
+                self.fileDir.setItemExpanded(eachDepend, 1)
+
+        else:
+            self.fileDir.expandAll()
 
     def collapseFolder(self):
-        pass
+        """
+        Collapse Folder action
+        :return: None
+        """
+        currentItem = self.fileDir.invisibleRootItem()
+
+        if self.fileDir.selectedItems():
+            currentItem = self.fileDir.selectedItems()[-1]
+
+        self.dependentList = [currentItem]
+        self.collectChildItems(currentItem)
+        for eachDepend in self.dependentList:
+            self.fileDir.collapseItem(eachDepend)
+
+    def collectChildItems(self, parent):
+        """
+        Collect child items
+        :param parent: str, selected item
+        :return: None
+        """
+        for index in range(parent.childCount()):
+            currentChild = parent.child(index)
+            self.dependentList.append(currentChild)
+            self.collectChildItems(currentChild)
 
     def removeFolder(self):
-        pass
+        """
+        remove folder from folder structure
+        :return: None
+        """
+        selectedItems = self.fileDir.selectedItems()
+        if selectedItems:
+            replay = QtWidgets.QMessageBox.question(self, 'Question',
+                                                    'Are you sure, you want to remove the folder?',
+                                                    QtWidgets.QMessageBox.Yes, QtWidgets.QMessageBox.Cancel)
+            logger.warning('Are you sure you want to remove the folder?')
+
+            if replay == QtWidgets.QMessageBox.Yes:
+                for eachItem in selectedItems:
+                    folderPath = str(eachItem.toolTip(0))
+                    try:
+                        os.chmod(folderPath, 0777)
+                        shutil.rmtree(folderPath)
+                        logger.info('Remove folder Path: \t%s !' % folderPath)
+                    except Exception, result:
+                        logger.warning(result)
+
+                # refresh folders structure
+                self.removeExistWidget(self.animWidgetLayout)
+                self.loadFolderStructure(self.dataDir)
+        else:
+            QtWidgets.QMessageBox.warning(self, 'Warning',
+                                          'No folder selected!\nPlease select the folder!',
+                                          QtWidgets.QMessageBox.Ok)
+            logger.warning('No folder selected!\nPlease select the folder!')
+
+    def removeExistWidget(self, layout):
+        """
+        remove Exist widget(s) from animGridLayout
+        :param layout: QGridLayout, self.animGridLayout
+        :return: None
+        """
+        for index in range(layout.count()):
+            if layout.itemAt(index).widget():
+                layout.itemAt(index).widget().deleteLater()
 
     def renameFolder(self):
-        pass
+        """
+        Rename selected folder
+        :return: None
+        """
+        selectedItems = self.fileDir.selectedItems()
+        if selectedItems:
+            newName, ok = QtWidgets.QInputDialog.getText(self, 'Folder Name',
+                                                         'Enter a new name: ',
+                                                         QtWidgets.QLineEdit.Normal)
+
+            if ok:
+                currentFolderPath = str(selectedItems[-1].toolTip(0))
+
+                newFolderPath = '%s/%s' % (os.path.dirname(currentFolderPath), str(newName))
+
+                replay = 0
+                try:
+                    os.chmod(currentFolderPath, 0777)
+                    os.rename(currentFolderPath, newFolderPath)
+                    replay = 1
+                except Exception, result:
+                    logger.warning(result)
+                    replay = 0
+
+                if replay:
+                    selectedItems[-1].setText(0, newName)
+                    selectedItems[-1].setToolTip(0, newFolderPath)
+
+        else:
+            QtWidgets.QMessageBox.warning(self, 'Warning',
+                                          'No folder selected!\nPlease select the folder!',
+                                          QtWidgets.QMessageBox.Ok)
+            logger.warning('No folder selected!\nPlease select the folder!')
 
     def onFolderContextMenu(self, paint):
         """
@@ -303,32 +472,198 @@ class MainUI(QtWidgets.QDialog):
         self.folderMenu.exec_(self.fileDir.mapToGlobal(paint))
 
     def recordAnim(self):
-        pass
+        """
+        Record anim for saving animation
+        :return: None
+        """
+        if self.currentMode == 'export':
+            if os.path.isfile(self.tempGIFDir):
+                try:
+                    os.chmod(self.tempGIFDir, 0777)
+                    os.remove(self.tempGIFDir)
+
+                except Exception, result:
+                    logger.warning(result)
+
+            modelPanelList = cmds.getPanel(type='modelPanel')
+            for eachModelPanel in modelPanelList:
+                cmds.modelEditor(eachModelPanel, e=1, alo=0)
+                cmds.modelEditor(eachModelPanel, e=1, pm=1)
+
+            startFrame = cmds.playbackOptions(min=1, q=1)
+            endFrame = cmds.playbackOptions(max=1, q=1)
+
+            tempImageList = list()
+            for i in range(int(startFrame), int(endFrame+1)):
+                tempImage = cmds.playblast(st=i, et=i, fmt='image', cc=1, v=0, orn=0, fp=1, p=100, c='png',
+                                           wh=[512, 512], cf='%s/tempImg_%s.png' % (self.tempDir, i))
+                tempImageList.append(tempImage)
+
+            # make GIF from tempImageList
+            frames = list()
+            for tempImage in tempImageList:
+                frames.append(imageio.imread(tempImage))
+            imageio.mimsave(self.tempGIFDir, frames, 'GIF', duration=0.1)
+
+            # remove temp images
+            for i in tempImageList:
+                if os.path.isfile(i):
+                    try:
+                        os.chmod(i, 0777)
+                        os.remove(i)
+                    except Exception, result:
+                        logger.warning(result)
+
+            self.loadGIF2Button(self.tempGIFDir, self.recordBtn)
+
+    def loadGIF2Button(self, path, button):
+        self.movie = QtGui.QMovie(path)
+        self.movie.frameChanged.connect(partial(self.setBtnIcon, button))
+
+        self.movie.start()
+
+    def setBtnIcon(self, button, *args):
+        button.setIcon(QtGui.QIcon(self.movie.currentPixmap()))
+        button.setIconSize(QtCore.QSize(self.size[0], self.size[1]))
 
     def saveAnim(self):
-        pass
+        """
+        Save pose to specified directory
+        :return: None
+        """
+        animLabel = str(self.nameEditLine.text())
+        selectionList = cmds.ls(sl=1)
+        if animLabel:
+            if selectionList:
+                currentItem = self.fileDir.selectedItems()
+                if currentItem:
+                    # export anim curve
+
+                    currentAnimGIFPath = exportAnimCurve.exportAnimCurve(selectionList, currentItem[-1].toolTip(0),
+                                                                         animLabel, self.tempGIFDir, self.iconsDir)
+
+                    self.nameEditLine.clear()
+                    self.loadGIF2Button(path=currentAnimGIFPath, button=self.recordBtn)
+
+                    # refresh
+                    self.loadCurrentFolder()
+                    logger.info('Successfully Save Anim Curve')
+                else:
+                    QtWidgets.QMessageBox.warning(self, 'Warning', 'No folder selected!\nPlease select the folder!',
+                                                  QtWidgets.QMessageBox.Ok)
+                    logger.warning('No folder selected!\nPlease select the folder!')
+            else:
+                QtWidgets.QMessageBox.warning(self, 'Warning',
+                                              'No item selected!\nPlease select the Control Object(s)!',
+                                              QtWidgets.QMessageBox.Ok)
+                logger.warning('No item selected!\nPlease select the Control Object(s)!')
+        else:
+            QtWidgets.QMessageBox.warning(self, 'Warning',
+                                          'No pose file name enter!\nPlease input the pose file name!',
+                                          QtWidgets.QMessageBox.Ok)
+            logger.warning('No pose file name enter!\nPlease input the pose file name!')
 
     def loadCurrentFolder(self):
-        pass
-
-    def renamePose(self):
-        pass
-
-    def poseExportImportSwitch(self, mode):
         """
-        Pose mode for export and import switch
+        Load anim(s) and icon(s) of current folder
+        :return: None
+        """
+        self.dependentList = []
+
+        currentItems = self.fileDir.selectedItems()
+
+        for eachItem in currentItems:
+            self.dependentList.append(eachItem)
+            self.collectChildItems(eachItem)
+
+        # clean layout
+        self.removeExistWidget(self.animWidgetLayout)
+        # add toolButtons to layout
+        self.loadAnim2Layout(self.dependentList)
+
+        # switch to export mode
+        self.animExportImportSwitch('export')
+
+    def loadAnim2Layout(self, itemList):
+        """
+        load anim(s) to animGridLayout
+        :param itemList: list(QToolButton),
+        :return: None
+        """
+        animList = []
+
+        for eachItem in itemList:
+            currentPath = str(eachItem.toolTip(0))
+            if os.path.isdir(currentPath):
+                directoryList = os.listdir(currentPath)
+
+                for eachFile in directoryList:
+                    if os.path.isfile('%s/%s' % (currentPath, eachFile)):
+                        if eachFile.endswith('.anim'):
+                            animList.append('%s/%s' % (currentPath, eachFile))
+
+        row = -1
+        column = 0
+        coordinateList = []
+        for index in range(len(animList)):
+            if index % 4:
+                column += 1
+                coordinateList.append([row, column])
+            else:
+                row += 1
+                column = 0
+                coordinateList.append([row, column])
+
+        # tool buttons
+        for index in range(len(animList)):
+            animLabel = os.path.splitext(os.path.basename(animList[index]))[0]
+
+            # tool button
+            toolButton = QtWidgets.QToolButton(self.animWidget)
+            toolButton.setFixedSize(90, 90)
+            toolButton.setObjectName('toolButton_%s' % animLabel)
+            toolButton.setText(animLabel)
+            toolButton.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+
+            # Icons
+            animIconPath  = animList[index].replace('.anim', '.png')
+            icon = QtGui.QIcon()
+            icon.addPixmap(QtGui.QPixmap(animIconPath), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            toolButton.setIcon(icon)
+            toolButton.setIconSize(QtCore.QSize(80, 70))
+
+            self.animWidgetLayout.addWidget(toolButton, coordinateList[index][0], coordinateList[index][1], 1, 1)
+
+            # import anims
+            toolButton.clicked.connect(partial(self.setCurrentAnim, animList[index]))
+
+    def setCurrentAnim(self, animPath):
+        pass
+
+    def renameAnim(self):
+        pass
+
+    def animExportImportSwitch(self, mode):
+        """
+        Anim mode for export and import switch
         :param mode: str, 'export' or 'import'
         :return: None
         """
-        pass
+        self.saveBtn.hide()
+        # self.sliderGroup.hide()
 
+        if mode == 'export':
+            self.nameEditLine.clear()
+            self.historyEditLine.clear()
 
+            self.saveBtn.show()
+            self.loadGIF2Button(button=self.recordBtn, path='%s/snapshot.png' % self.iconsDir)
 
-class gifButton(QtWidgets.QPushButton):
-    def __init__(self, parent=None):
-        QtWidgets.QPushButton.__init__(self, parent)
+            self.currentMode = 'export'
 
-        self.label = QtWidgets.QLabel()
+        if mode == 'import':
+            self.sliderGroup.show()
 
+            self.currentMode = 'import'
 
 

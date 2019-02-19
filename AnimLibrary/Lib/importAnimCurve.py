196c7a2393e3a9ce3186d775db5e1db2bea879f4
Yixiong Xu <328665042@qq.com>
2019-02-19 17:55:06 +0800

    Import Animation Curve

--- AnimLibrary/Lib/importAnimCurve.py
+++ AnimLibrary/Lib/importAnimCurve.py
@@ -1,38 +1,38 @@
 import maya.api.OpenMaya as om
 import maya.api.OpenMayaAnim as oma
-import maya.cmds as cmds
 import json
 
-filePath = 'C:/Users/user/Desktop/AnimCurve.Anim'
 
-animData = open(filePath, 'r')
-data = json.load(animData)
-animData.close()
+def importAnimCurve(selectionList,
+                    animPath,
+                    ):
 
-selectionList = cmds.ls(sl=1)
-if data and selectionList:
-    for eachObject in selectionList:
-        for eachAttribute in data[eachObject]:
-            for eachAnimCurveAttribute in data[eachObject][eachAttribute]:
+    animData = open(animPath, 'r')
+    data = json.load(animData)
+    animData.close()
+
+    if data and selectionList:
+        for eachObject in selectionList:
+            for eachAnimCurveAttribute in data['animCurve'][eachObject]:
 
                 # get value
-                animCurveType = eachAnimCurveAttribute['animCurveType']
-                preInfinity = eachAnimCurveAttribute['preInfinity']
-                postInfinity = eachAnimCurveAttribute['postInfinity']
-                weightedTangents = eachAnimCurveAttribute['weightedTangents']
-                numKeys = eachAnimCurveAttribute['numKeys']
-                timeList = eachAnimCurveAttribute['timeList']
-                valueList = eachAnimCurveAttribute['valueList']
-                inTangentTypeList = eachAnimCurveAttribute['inTangentTypeList']
-                inTangentAngleList = eachAnimCurveAttribute['inTangentAngleList']
-                inTangentAngleWeightList = eachAnimCurveAttribute['inTangentAngleWeightList']
-                outTangentTypeList = eachAnimCurveAttribute['outTangentTypeList']
-                outTangentAngleList = eachAnimCurveAttribute['outTangentAngleList']
-                outTangentAngleWeightList = eachAnimCurveAttribute['outTangentAngleWeightList']
+                animCurveType = data['animCurve'][eachObject][eachAnimCurveAttribute]['animCurveType']
+                preInfinity = data['animCurve'][eachObject][eachAnimCurveAttribute]['preInfinity']
+                postInfinity = data['animCurve'][eachObject][eachAnimCurveAttribute]['postInfinity']
+                weightedTangents = data['animCurve'][eachObject][eachAnimCurveAttribute]['weightedTangents']
+                numKeys = data['animCurve'][eachObject][eachAnimCurveAttribute]['numKeys']
+                timeList = data['animCurve'][eachObject][eachAnimCurveAttribute]['timeList']
+                valueList = data['animCurve'][eachObject][eachAnimCurveAttribute]['valueList']
+                inTangentTypeList = data['animCurve'][eachObject][eachAnimCurveAttribute]['inTangentTypeList']
+                inTangentAngleList = data['animCurve'][eachObject][eachAnimCurveAttribute]['inTangentAngleList']
+                inTangentAngleWeightList = data['animCurve'][eachObject][eachAnimCurveAttribute]['inTangentAngleWeightList']
+                outTangentTypeList = data['animCurve'][eachObject][eachAnimCurveAttribute]['outTangentTypeList']
+                outTangentAngleList = data['animCurve'][eachObject][eachAnimCurveAttribute]['outTangentAngleList']
+                outTangentAngleWeightList = data['animCurve'][eachObject][eachAnimCurveAttribute]['outTangentAngleWeightList']
 
                 # convert current object and attribute to a new MPlug object
                 mSelectionList = om.MSelectionList()
-                mSelectionList.add(eachAnimCurveAttribute)
+                mSelectionList.add('%s.%s' % (eachObject, eachAnimCurveAttribute))
 
                 attributeMPlug = mSelectionList.getPlug(0)
 
@@ -63,7 +63,7 @@ if data and selectionList:
                 MDoubleValueList = om.MDoubleArray()
 
                 for index in range(len(timeList)):
-                    MTimeArray.append(om.MTime(timeList[index]), om.MTime.uiUnit())
+                    MTimeArray.append(om.MTime(timeList[index], om.MTime.uiUnit()))
                     MDoubleValueList.append(valueList[index])
 
                 MFnAnimCurve.addKeys(MTimeArray, MDoubleValueList, 0, 0, 1)
@@ -80,3 +80,12 @@ if data and selectionList:
 
                     MFnAnimCurve.setWeight(index, inTangentAngleWeightList[index], 1)
                     MFnAnimCurve.setWeight(index, outTangentAngleWeightList[index], 0)
+
+    # read history
+    historyData = data['history']
+    historyList = ['Owner: %s' % historyData[0],
+                   'Created: %s' % historyData[1],
+                   'Maya version: %s' % historyData[2],
+                   'Module version: %s' % historyData[3]]
+
+    return historyList
